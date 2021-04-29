462c18fd1fc799de72b30c1f6723ca26a58f7ba1
Herinta Armantya <herintya@gmail.com>
2021-04-29 12:17:45 +0700

    first commit

---
+++ CRUD_Herintaa/Form1.Designer.cs
@@ -0,0 +1,360 @@
+﻿
+namespace CRUD_Herintaa
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel = new System.Windows.Forms.Panel();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.browse = new System.Windows.Forms.Button();
+            this.namaBarang = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.jumlahBarang = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.asalPabrik = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.deskripsi = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.search = new System.Windows.Forms.TextBox();
+            this.db_produk = new CRUD_Herintaa.db_produk();
+            this.dataprodukBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.data_produkTableAdapter = new CRUD_Herintaa.db_produkTableAdapters.data_produkTableAdapter();
+            this.iDDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.namaBarangDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.jumlahBarangDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.asalPabrikDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.deskripsiDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.imageDataGridViewImageColumn = new System.Windows.Forms.DataGridViewImageColumn();
+            this.panel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.db_produk)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataprodukBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel
+            // 
+            this.panel.Controls.Add(this.label4);
+            this.panel.Controls.Add(this.deskripsi);
+            this.panel.Controls.Add(this.label3);
+            this.panel.Controls.Add(this.asalPabrik);
+            this.panel.Controls.Add(this.label2);
+            this.panel.Controls.Add(this.jumlahBarang);
+            this.panel.Controls.Add(this.label1);
+            this.panel.Controls.Add(this.namaBarang);
+            this.panel.Controls.Add(this.browse);
+            this.panel.Controls.Add(this.pictureBox);
+            this.panel.Location = new System.Drawing.Point(12, 21);
+            this.panel.Name = "panel";
+            this.panel.Size = new System.Drawing.Size(627, 242);
+            this.panel.TabIndex = 0;
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.DataBindings.Add(new System.Windows.Forms.Binding("Image", this.dataprodukBindingSource, "Image", true));
+            this.pictureBox.Location = new System.Drawing.Point(21, 24);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(117, 149);
+            this.pictureBox.TabIndex = 0;
+            this.pictureBox.TabStop = false;
+            // 
+            // browse
+            // 
+            this.browse.Location = new System.Drawing.Point(33, 190);
+            this.browse.Name = "browse";
+            this.browse.Size = new System.Drawing.Size(93, 35);
+            this.browse.TabIndex = 1;
+            this.browse.Text = "Browse";
+            this.browse.UseVisualStyleBackColor = true;
+            this.browse.Click += new System.EventHandler(this.browse_Click);
+            // 
+            // namaBarang
+            // 
+            this.namaBarang.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.dataprodukBindingSource, "NamaBarang", true));
+            this.namaBarang.Location = new System.Drawing.Point(260, 33);
+            this.namaBarang.Name = "namaBarang";
+            this.namaBarang.Size = new System.Drawing.Size(304, 20);
+            this.namaBarang.TabIndex = 0;
+            this.namaBarang.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(182, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Nama Barang";
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.AutoGenerateColumns = false;
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.iDDataGridViewTextBoxColumn,
+            this.namaBarangDataGridViewTextBoxColumn,
+            this.jumlahBarangDataGridViewTextBoxColumn,
+            this.asalPabrikDataGridViewTextBoxColumn,
+            this.deskripsiDataGridViewTextBoxColumn,
+            this.imageDataGridViewImageColumn});
+            this.dataGridView.DataSource = this.dataprodukBindingSource;
+            this.dataGridView.Location = new System.Drawing.Point(12, 304);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.Size = new System.Drawing.Size(627, 165);
+            this.dataGridView.TabIndex = 1;
+            this.dataGridView.KeyDown += new System.Windows.Forms.KeyEventHandler(this.dataGridView_KeyDown);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(250, 478);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(93, 35);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "New";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(349, 478);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(93, 35);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Edit";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(448, 478);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(93, 35);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Cancel";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(547, 478);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(93, 35);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "Save";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(182, 78);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Jumlah Barang";
+            // 
+            // jumlahBarang
+            // 
+            this.jumlahBarang.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.dataprodukBindingSource, "JumlahBarang", true));
+            this.jumlahBarang.Location = new System.Drawing.Point(260, 75);
+            this.jumlahBarang.Name = "jumlahBarang";
+            this.jumlahBarang.Size = new System.Drawing.Size(304, 20);
+            this.jumlahBarang.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(182, 122);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Asal Pabrik";
+            // 
+            // asalPabrik
+            // 
+            this.asalPabrik.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.dataprodukBindingSource, "AsalPabrik", true));
+            this.asalPabrik.Location = new System.Drawing.Point(260, 119);
+            this.asalPabrik.Name = "asalPabrik";
+            this.asalPabrik.Size = new System.Drawing.Size(304, 20);
+            this.asalPabrik.TabIndex = 2;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(182, 168);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(50, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Deskripsi";
+            // 
+            // deskripsi
+            // 
+            this.deskripsi.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.dataprodukBindingSource, "Deskripsi", true));
+            this.deskripsi.Location = new System.Drawing.Point(260, 165);
+            this.deskripsi.Multiline = true;
+            this.deskripsi.Name = "deskripsi";
+            this.deskripsi.Size = new System.Drawing.Size(304, 60);
+            this.deskripsi.TabIndex = 3;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(14, 275);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(41, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Seacrh";
+            // 
+            // search
+            // 
+            this.search.Location = new System.Drawing.Point(92, 272);
+            this.search.Name = "search";
+            this.search.Size = new System.Drawing.Size(547, 20);
+            this.search.TabIndex = 1;
+            this.search.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.search_KeyPress);
+            // 
+            // db_produk
+            // 
+            this.db_produk.DataSetName = "db_produk";
+            this.db_produk.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // dataprodukBindingSource
+            // 
+            this.dataprodukBindingSource.DataMember = "data_produk";
+            this.dataprodukBindingSource.DataSource = this.db_produk;
+            // 
+            // data_produkTableAdapter
+            // 
+            this.data_produkTableAdapter.ClearBeforeFill = true;
+            // 
+            // iDDataGridViewTextBoxColumn
+            // 
+            this.iDDataGridViewTextBoxColumn.DataPropertyName = "ID";
+            this.iDDataGridViewTextBoxColumn.HeaderText = "ID";
+            this.iDDataGridViewTextBoxColumn.Name = "iDDataGridViewTextBoxColumn";
+            // 
+            // namaBarangDataGridViewTextBoxColumn
+            // 
+            this.namaBarangDataGridViewTextBoxColumn.DataPropertyName = "NamaBarang";
+            this.namaBarangDataGridViewTextBoxColumn.HeaderText = "NamaBarang";
+            this.namaBarangDataGridViewTextBoxColumn.Name = "namaBarangDataGridViewTextBoxColumn";
+            // 
+            // jumlahBarangDataGridViewTextBoxColumn
+            // 
+            this.jumlahBarangDataGridViewTextBoxColumn.DataPropertyName = "JumlahBarang";
+            this.jumlahBarangDataGridViewTextBoxColumn.HeaderText = "JumlahBarang";
+            this.jumlahBarangDataGridViewTextBoxColumn.Name = "jumlahBarangDataGridViewTextBoxColumn";
+            // 
+            // asalPabrikDataGridViewTextBoxColumn
+            // 
+            this.asalPabrikDataGridViewTextBoxColumn.DataPropertyName = "AsalPabrik";
+            this.asalPabrikDataGridViewTextBoxColumn.HeaderText = "AsalPabrik";
+            this.asalPabrikDataGridViewTextBoxColumn.Name = "asalPabrikDataGridViewTextBoxColumn";
+            // 
+            // deskripsiDataGridViewTextBoxColumn
+            // 
+            this.deskripsiDataGridViewTextBoxColumn.DataPropertyName = "Deskripsi";
+            this.deskripsiDataGridViewTextBoxColumn.HeaderText = "Deskripsi";
+            this.deskripsiDataGridViewTextBoxColumn.Name = "deskripsiDataGridViewTextBoxColumn";
+            // 
+            // imageDataGridViewImageColumn
+            // 
+            this.imageDataGridViewImageColumn.DataPropertyName = "Image";
+            this.imageDataGridViewImageColumn.HeaderText = "Image";
+            this.imageDataGridViewImageColumn.Name = "imageDataGridViewImageColumn";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(651, 520);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.search);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dataGridView);
+            this.Controls.Add(this.panel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Data Produk";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel.ResumeLayout(false);
+            this.panel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.db_produk)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataprodukBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox namaBarang;
+        private System.Windows.Forms.Button browse;
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox deskripsi;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox asalPabrik;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox jumlahBarang;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox search;
+        private db_produk db_produk;
+        private System.Windows.Forms.BindingSource dataprodukBindingSource;
+        private db_produkTableAdapters.data_produkTableAdapter data_produkTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn iDDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn namaBarangDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn jumlahBarangDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn asalPabrikDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn deskripsiDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewImageColumn imageDataGridViewImageColumn;
+    }
+}
+
---
+++ CRUD_Herintaa/Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CRUD_Herintaa
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void search_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)13)
+            {
+                if (string.IsNullOrEmpty(search.Text))
+                    dataGridView.DataSource = dataprodukBindingSource;
+                else
+                {
+                    var query = from o in this.db_produk.data_produk
+                                where o.NamaBarang.Contains(search.Text) || o.JumlahBarang == search.Text || o.AsalPabrik == search.Text || o.Deskripsi.Contains(search.Text)
+                                select o;
+                    dataGridView.DataSource = query.ToList();
+                }
+            }
+        }
+
+        private void dataGridView_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Delete)
+            {
+                if (MessageBox.Show("Are you sure want to delete this record?", "Message", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                    dataprodukBindingSource.RemoveCurrent();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e) //Button New
+        {
+            try
+            {
+                panel.Enabled = true;
+                namaBarang.Focus();
+                this.db_produk.data_produk.Adddata_produkRow(this.db_produk.data_produk.Newdata_produkRow());
+                dataprodukBindingSource.MoveLast();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                dataprodukBindingSource.ResetBindings(false);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e) //button edit
+        {
+            panel.Enabled = true;
+            namaBarang.Focus();
+        }
+
+        private void button4_Click(object sender, EventArgs e) //Button Cancel
+        {
+            panel.Enabled = false;
+            dataprodukBindingSource.ResetBindings(false);
+        }
+
+        private void button5_Click(object sender, EventArgs e) //Button save
+        {
+            try
+            {
+                dataprodukBindingSource.EndEdit();
+                data_produkTableAdapter.Update(this.db_produk.data_produk);
+                panel.Enabled = false;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                dataprodukBindingSource.ResetBindings(false);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'db_produk.data_produk' table. You can move, or remove it, as needed.
+            this.data_produkTableAdapter.Fill(this.db_produk.data_produk);
+            dataprodukBindingSource.DataSource = this.db_produk.data_produk;
+
+        }
+
+        private void browse_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (OpenFileDialog ofd = new OpenFileDialog() { Filter = "JPEG|*.jpg", ValidateNames = true, Multiselect = false })
+                {
+                    if (ofd.ShowDialog() == DialogResult.OK)
+                        pictureBox.Image = Image.FromFile(ofd.FileName);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
